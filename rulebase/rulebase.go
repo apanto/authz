293fd64970c5fbb9401c267b50de76860d2ce365
Alex <apapantonatos@gmail.com>
2018-01-07 13:29:16 +0100

    restructured rulebase.go. Implemented new Rulebase type and attached all relevant function to the type. Now Rulebase fully encapsulates prefixtree and toplevel doesn't need to include prefeixtree.

--- rulebase/rulebase.go
+++ rulebase/rulebase.go
@@ -19,56 +19,103 @@ const (
 	UPDATE = 16
 )
 
-type Rule struct {
-	Url string              `yaml:"Url"`
-	ACL map[string][]string `yaml:"ACL"`
+type Rulebase struct {
+	tree                 *prefixtree.Tree
+	default_access_flags int
 }
 
-//Creates a rulebase from an array of "Rules"
-func Create(rules *[]Rule) (*prefixtree.Tree, error) {
-	tree := prefixtree.New()
+type Rule struct {
+	Url string
+	ACL map[string][]string
+}
 
-	for _, r := range *rules {
-		for subject, access := range r.ACL {
-			// fmt.Printf("user: %s -> url: %s access(%T): %s\n", subject, r.Url, access, access)
-			access_flags := 0
-			for _, v := range access {
-				switch v {
-				case "GET":
-					access_flags += GET
-				case "PUT":
-					access_flags += PUT
-				case "POST":
-					access_flags += POST
-				case "DELETE":
-					access_flags += DELETE
-				case "UPDATE":
-					access_flags += UPDATE
-				default:
-					return nil, errors.New(fmt.Sprintf("Unknown HTTP verb %s\n", v))
-				}
-			}
-			// fmt.Printf("user: %s -> url: %s access: %s\n", subject, r.Url, access_flags)
-			err := tree.AddKey(r.Url, subject, access_flags)
-			if err != nil {
-				return nil, err
-			}
+//Creates a new empty rulebase
+func New() *Rulebase {
+	var rb Rulebase
+	rb.tree = prefixtree.New()
+	rb.SetDefaultAccess([]string{})
+	return &rb
+}
+
+//Sets the access flags for the default access policy. Expects and array of HTTP verbs as strings
+func (rb Rulebase) SetDefaultAccess(access []string) error {
+	access_flags := 0
+	for _, v := range access {
+		switch v {
+		case "GET":
+			access_flags += GET
+		case "PUT":
+			access_flags += PUT
+		case "POST":
+			access_flags += POST
+		case "DELETE":
+			access_flags += DELETE
+		case "UPDATE":
+			access_flags += UPDATE
+		default:
+			return errors.New(fmt.Sprintf("Unknown HTTP verb %s\n", v))
 		}
 	}
 
-	// fmt.Println(*tree.Digraph())
-	return tree, nil
+	rb.default_access_flags = access_flags
+	return nil
 }
 
-//TODO: make default access policy configurable
-func Lookup(subject string, url string, rb *prefixtree.Tree) (int, error) {
-	v, err := rb.Match(url, subject)
+//Creates a new rulebase from an array of rules
+func Create(rules *[]Rule) (*Rulebase, error) {
+	rb := New()
+
+	for _, r := range *rules {
+		err := rb.Add(&r)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return rb, nil
+}
+
+//Adds a rule to a rulebase
+func (rb Rulebase) Add(r *Rule) error {
+	for subject, access := range r.ACL {
+		// fmt.Printf("user: %s -> url: %s access(%T): %s\n", subject, r.Url, access, access)
+		access_flags := 0
+		for _, v := range access {
+			switch v {
+			case "GET":
+				access_flags += GET
+			case "PUT":
+				access_flags += PUT
+			case "POST":
+				access_flags += POST
+			case "DELETE":
+				access_flags += DELETE
+			case "UPDATE":
+				access_flags += UPDATE
+			default:
+				return errors.New(fmt.Sprintf("Unknown HTTP verb %s\n", v))
+			}
+		}
+		// fmt.Printf("user: %s -> url: %s access: %s\n", subject, r.Url, access_flags)
+		err := rb.tree.AddKey(r.Url, subject, access_flags)
+		if err != nil {
+			return err
+		}
+	}
+
+	// fmt.Println(*rb.tree.Digraph())
+	return nil
+}
+
+//Looks up a subject and url in the rulebase and returns the access flags as int
+func (rb Rulebase) Lookup(subject string, url string) (int, error) {
+	v, err := rb.tree.Match(url, subject)
 	if err != nil {
 		if err.Error() == "index does not exist" {
-			//If the subject is not present in the ACL for this prefix the default access policy is DENY
-			return DENY, nil
+			//If the subject is not present in the ACL for this prefix return the default access flags of this rb
+			return rb.default_access_flags, nil
 		} else {
-			return DENY, err
+			return 0, err
 		}
 	}
 	return v, nil
